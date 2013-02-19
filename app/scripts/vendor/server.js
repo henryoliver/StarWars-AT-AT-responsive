58216cf5ca7a78605c8df064984e06486da2953d
Henry Oliver <henry_oliver@me.com>
2013-02-19 19:50:22 -0300

    Update #3424 by Henry: Update the yeoman and the project scaffold

---
+++ app/scripts/vendor/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var lactate = require('lactate'),
+    http = require('http'),
+    port = 80,
+    root = './';
+
+var options = {
+    root: root,
+    from: '',
+    not_found: '404.html',
+    subdirs: true,
+    //autoindex: true,
+    debug: true
+  };
+
+var files = lactate.dir(root, options);
+
+var server = new http.Server();
+
+server.addListener('request', function(req, res) {
+  if (req.url === '/') {
+    files.serve('index.html', req, res);
+  } else {
+    files.serve(req, res);
+  }
+});
+
+server.listen(port);
+
+console.log('Listening on port', port);
