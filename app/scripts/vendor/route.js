180029ea5306a394f911d53a3eded38ef72cd400
Henry Oliver <henry_oliver@me.com>
2013-02-20 17:07:49 -0300

    Update #4343 by Henry: Update the deploy status

--- app/scripts/vendor/route.js
+++ app/scripts/vendor/route.js
@@ -4,14 +4,14 @@ var bouncy = require('bouncy');
 
 var server = bouncy(function (req, res, bounce) {
     if (req.headers.host === 'polen.is') {
-        bounce(8001);
+        bounce(8000);
     }
     else if (req.headers.host === 'app.polen.is') {
-        bounce(8002);
+        bounce(8001);
     }
     else {
         res.statusCode = 404;
         res.end('Bouncy says: no such host');
     }
 });
-server.listen(8000);
+server.listen(80);
